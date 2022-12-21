8e390a60076a2261fb9e331a52e5789cb6454173
HocLT <hdquang6@gmail.com>
2022-12-21 18:40:45 +0700

    add Details.cs

---
+++ Assets/Scripts/Details.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character
+{
+    public string Name { get; set; }
+    
+    public Character(string name)
+    {
+        Name = name;
+        Debug.Log("The player name is: " + name);
+    }
+}
+
+public class Details : MonoBehaviour
+{
+    Character character;
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = new Character("Quang");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
